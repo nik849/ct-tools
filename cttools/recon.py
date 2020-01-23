3ca5b06e87b41c6b1f8c0698ae41d8df1d417674
Nick Hale <nickhale@protonmail.ch>
2020-01-23 08:23:16 +0000

    Interim commit - removed ray.

--- cttools/recon.py
+++ cttools/recon.py
@@ -52,39 +52,20 @@ def fdk_slice(projections, config, slice):
     #return out
 
 
-@ray.remote
-def _fdk_slice(projection, angle, config, slice):
-
-    x_proj = projection[0].T[0, :]
-    y_proj = projection[0].T[:, 0]
-    z = 0
-    proj_width = projection[0].shape[0]
-    proj_height = projection[0].shape[1]
-    recon = np.zeros((len(x_proj), len(y_proj)), dtype=np.float32)
-    radius = proj_width / 2.
-    x = np.arange(proj_width) - radius
-    projection[0][0] += config.center_of_rot_y
-    x_r, y_r = np.mgrid[:config.n_voxels_x, :config.n_voxels_y] - radius
-    t = x_r * np.cos(angle) - y_r * np.sin(angle)
-    if slice is not None:
-        interpolant = partial(np.interp, xp=x, fp=projection[0][:, int(slice)], left=0, right=0)
-    else:
-        interpolant = partial(np.interp, xp=x, fp=projection[0][:, int(proj_height / 2)], left=0, right=0)
-    recon = recon + interpolant(t)
-    return recon
-
-
 def fdk_vol(projections, config, **kwargs):
     output_file = 'output.raw'
     with open(output_file, 'wb') as f:
-        ray.init()
+        pool = Pool()
+        #ray.init()
         temp = []
-        num_imgs = int(config.n_voxels_z)
-        for imgs in list(range(num_imgs)):
-            temp.append(fdk_slice.remote(projections, config, slice=img))
-        for slice in tqdm(temp, total=len(temp)):
-            f.write(ray.get(slice))
-    ray.shutdown()
+        func = partial(fdk_slice, projections, config)
+        num_imgs = list(range(int(config.n_voxels_z)))
+        print(f'Processing {config.n_voxels_z} slices ...')
+        for slice in pool.imap(func, slice=slice)
+            temp.append(slice)
+        print(f'Writing out ...')
+        for slice in tqdm(sorted(temp), total=len(temp)):
+            f.write(slice)
 
 
 def read_projections(path, into_ram=True, flat_corrected=True, n_proj=3142):
